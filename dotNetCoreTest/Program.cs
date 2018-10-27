d52bad5c64bc1f711fa5b311b7e533e3a3c1d59c
Ariel Ornstein <arielorn@gmail.com>
2018-10-27 11:43:43 +0300

    Initial commit

---
+++ dotNetCoreTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using System;
+using System.Linq;
+
+namespace dotNetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (IDocumentStore store = new DocumentStore()
+            {
+                Url =  "http://localhost:8080"
+            }.Initialize())
+            {
+                var session = store.OpenSession(new OpenSessionOptions()
+                {
+                    Database = "PersonsCore"
+                });
+
+                session.Store(new Person
+                {
+                    FirstName = "Ariel",
+                    LastName = "Ornstein"
+                });
+
+                session.SaveChanges();
+                var person = session.Query<Person>().ToList();
+
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
